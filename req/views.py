bef502d765642e4022f0752b4580ea00321a72dd
Александр Сырокласов <SiroklasovA@rkvv.local>
2020-07-02 16:51:08 +0300

    123

--- req/views.py
+++ req/views.py
@@ -42,7 +42,8 @@ class ReqView(ListCreateAPIView):
             serializer.save()
             TOKEN='1191171470:AAFD2RFpUR0-W_RTqO4uco2WpCAZOCT1b4M'
             bot=telebot.TeleBot(TOKEN)
-            text='*Поступление новой заявки*\n\n'+request.data.get('shortname')+'\n\n_Автор:_ '+request.get('user').get('surname')+'\n_Телефон:_ '+request.data.get('phone')
+            text='*Поступление новой заявки*\n\n'+request.data.get('shortname')+'\n\n_Автор:_ '+request.data.get('user').get('surname')+'\n_Телефон:_ '+request.data.get('phone')
+            import pdb;pdb.set_trace()
             bot.send_message('-488020289',text,parse_mode="Markdown")
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
